a197dcbe62661ec4801e90f7081f7c196d61a850
Gabriel Roberge <45238094+GRob27@users.noreply.github.com>
2021-02-10 05:16:38 -0500

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import "./App.css";
+
+type AppProps = {};
+
+const App: FunctionComponent<AppProps> = () => {
+  return <div className="app">Hello world</div>;
+};
+
+export default App;
